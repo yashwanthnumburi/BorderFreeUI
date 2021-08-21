9b454fed2932aa36646ad93bbf224295ba9f5022
Yashwanth <yashwanthsa@gmail.com>
2021-08-22 00:41:19 +0530

    added heroku link

--- src/components/Product/productionActions.js
+++ src/components/Product/productionActions.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const getProductDetails=()=>{
-    return axios.get('http://localhost:3001/getallproducts').then(response=>{
+    return axios.get('https://evening-forest-19188.herokuapp.com/getallproducts').then(response=>{
         return response.data.Item;
     })
 };
 
 export const addProductToCart=(req)=>{
-    return axios.post('http://localhost:3001/addProductToCart',req).then(response=>{
+    return axios.post('https://evening-forest-19188.herokuapp.com/addProductToCart',req).then(response=>{
         console.log(response);
         return response;
     });
--- src/homeActions.js
+++ src/homeActions.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const getCartProducts=()=>{
-    return axios.get('http://localhost:3001/getCartProducts').then(response=>{
+    return axios.get('https://evening-forest-19188.herokuapp.com/getCartProducts').then(response=>{
         return response.data.Item;
     })
 };
 
 export const getItemDetailsInCart=(products)=>{
-    return axios.post('http://localhost:3001/getItemDetailsInCart',{products:products}).then(response=>{
+    return axios.post('https://evening-forest-19188.herokuapp.com/getItemDetailsInCart',{products:products}).then(response=>{
         return response.data
     })
 };
